545d0486b99edf4ef922929ecea1806ebeaa56ec
SuperQuentin <36150436+SuperQuentin@users.noreply.github.com>
2020-09-18 07:38:55 +0200

    Code go brrrrr

--- src/public/index.php
+++ src/public/index.php
@@ -1,48 +1,7 @@
 <?php
-    session_start();
-    require_once "../src/models/TrafficLight.php";
+    require "../Autoloader.php";
 
-    $url = '';
+    $webRouter = new App\Routers\Router($_SERVER['REQUEST_URI']);
 
-    if(isset($_GET['url'])){
-        $url = explode('/', $_GET['url']);
-    }
-
-    if($url[0] == 'next' && empty($url[1])){
-
-    }
-
-
-    $trafficLights = isset($_SESSION['traffic-lights']) ? unserialize($_SESSION['traffic-lights']) : null;
-
-    if(empty($trafficLights)){
-        $trafficLights = array(new TrafficLight());
-    }
-
-    array_push($trafficLights, new TrafficLight());
-    var_dump($trafficLights);
-
-?>
-
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Feu tri color</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-
-<body>
-    <?php
-        foreach ($trafficLights as $tl){
-    ?>
-            <?= $tl->build(); ?>
-    <?php
-        }
-    ?>
-</body>
-
-</html>
-
-<?php
-    $_SESSION['traffic-lights'] = serialize($trafficLights);
-?>
+    $webRouter->get(['/','App\Controller\HomeController@index']);
+    $webRouter->get(['/next','TrafficLightsController@next']);
